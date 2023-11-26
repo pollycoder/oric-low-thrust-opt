4037fbee2684ed63c23d22761df2f66f46b3dedc
Polly Zhou <polly@Pollys-MacBook-Pro.local>
2023-11-26 23:57:04 +0800

    add shooting

--- optimal_control/indirect_shooting.m
+++ optimal_control/indirect_shooting.m
@@ -5,10 +5,10 @@ clear all
 % LEO: omega = 4 rad/h
 % P3
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-global omega alpha rho A B x0 xf
+global omega alpha rho A B x0 xf t0 tf
 % Constant
 omega = 4;                                  % angular velocity, 4 rad/h
-alpha = 1e6;                                % Parameter to be adjusted
+alpha = 1e5;                                % Parameter to be adjusted
 rho = 10;                                   % Distance between chief and deputy
 
 % Initial and final states
@@ -23,31 +23,21 @@ M2 = diag([2 * omega, 0], 1) + diag([-2 * omega, 0], -1);
 A = [zeros(3), eye(3); M1, M2];
 B = [zeros(3); eye(3)];
 
-% Initial guess for the solution 2  
-% t - [t0, tf]
-% x - x0, [1,1,1,1,1,1]
-n = 50;
-tmesh = linspace(t0, tf, n);
-yguess = [8.521; -4.894; -1.854; -5.529; -8.154; -3.385; ones(6, 1)];
-solinit = bvpinit(tmesh, yguess);
-options = bvpset('Stats','on','RelTol',1e-5, 'Nmax', 100000);
+lambda_guess = [7.79e4; 9.37e4; 3.35e4; -456.09; 384.13; 165.61];
 
-sol = bvp4c(@bvpfun, @bvpbc, solinit, options);
+% x_sol is lambda
+x_sol = fsolve(@solver, lambda_guess);
 
-lambda = sol.y(7:12, :);
+lambda = x_sol;
+
+[t, y] = ode45(@bvpfun, [t0, tf], [x0', lambda']);
+lambda = y(:, 7:12)';
 u = B' * lambda;
 dJ = 0.5 .* (vecnorm(u) .* vecnorm(u));
-t = sol.x;
 J = trapz(t, dJ);
 fprintf('J = %f', J);
 
-% State - radius
-x1 = sol.y(1, :);
-x2 = sol.y(2, :);
-x3 = sol.y(3, :);
-x = sqrt(x1.^2 + x2.^2 + x3.^2);
-
-%% Plot
+% Plot
 subplot(2,2,1)
 plot(t, x, 'LineWidth', 1.5);
 title('State - pos');
@@ -88,7 +78,7 @@ text(x1(end), x2(end), x3(end), 'Arrival');hold on
 plot3(x1, x2, x3, 'k-', 'LineWidth', 1.5);hold on
 quiver3(x1, x2, x3, u(1, :), u(2, :), u(3, :), 0.3, 'Color', 'r','LineWidth', 1);
 title('Trajectory');
-
+%}
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Control equations
@@ -97,8 +87,18 @@ title('Trajectory');
 % x: state - v and a, 6x1 vector
 % lambda: costate, 6x1 vector
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function F = solver(x)
+global x0 xf t0 tf
+initial_guess = [x0', x'];
+options = odeset('MaxStep', 1e-4, 'RelTol',1e-5,'AbsTol',1e-9);
+[t, y] = ode45(@bvpfun, [t0, tf], initial_guess);
+s = length(t);
+F = y(s, 1:6) - xf';
+end
+
 function dydt = bvpfun(t, y)
 global rho alpha A B
+dydt = zeros(12, 1);
 syms x1 x2 x3 p1 p2 p3
 C =x1^2 + x2^2 + x3^2 - rho^2;
 C_value = double(subs(C, {x1, x2, x3}, {y(1), y(2), y(3)}));
@@ -107,13 +107,5 @@ gradC_value = double(subs(gradC, {x1, x2, x3, p1, p2, p3}, {y(1), y(2), y(3), y(
 dydt(1:6) = A * y(1:6) - B * B' * y(7:12);
 dydt(7:12) = -A' * y(7:12) - alpha * C_value * gradC_value;
 end
-
-% Boundary conditions
-function res = bvpbc(y0, yf)
-global x0 xf
-res = [y0(1:6) - x0; yf(1:6) - xf];
-end
-
-
 
 
