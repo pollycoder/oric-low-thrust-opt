4cebfde83c5baaf92454560c61b293415365b765
Polly Zhou <polly@polly-3.local>
2024-04-29 00:15:41 +0800

    add opt

--- optimal_control/indirect_lagrange_obj.m
+++ optimal_control/indirect_lagrange_obj.m
@@ -8,17 +8,20 @@
 clc;clear
 
 %-------------------------------------------------------------------%
-%---------------------------- Constant -----------------------------%
+%--------------------------- Optimization --------------------------%
 %-------------------------------------------------------------------%
-omega = 4;                                   % angular velocity, 4 rad/h
-rho0 = 10;                                   % Distance between chief and deputy
-rho = 11.7985884;
+rho_init = 11.78;
+[rho, J] = fminsearch(@obj_func, rho_init);
 
-% Initial and final states
-theta0 = pi;
-thetaf = theta0 + pi / 2;
-x0 = [rho0 * cos(theta0); rho0 * sin(theta0); 0; 0; 0; pi];
-xf = [rho0 * cos(thetaf); rho0 * sin(thetaf); 0; 0; 0; pi];
+
+%-------------------------------------------------------------------%
+%------------------------ Objective Function -----------------------%
+%-------------------------------------------------------------------%
+function J = obj_func(rho)
+
+%---------------------------- Constant -----------------------------%
+omega = 4;                                   % angular velocity, 4 rad/h
+rho_min = 8;
 
 % Time
 t0 = 0;
@@ -27,38 +30,24 @@ tf = 0.25;
 % Matrix
 M1 = diag([3 * omega^2, 0, -omega^2]);
 M2 = diag([2 * omega, 0], 1) + diag([-2 * omega, 0], -1);
-A = [zeros(3), eye(3); M1, M2];
-B = [zeros(3); eye(3)];
 
-
-%-------------------------------------------------------------------%
-%------------------------ Guess and Mesh ---------------------------%
-%-------------------------------------------------------------------%
+%------------------------- Guess and Mesh --------------------------%
 n = 1e2;
 tmesh = linspace(t0, tf, n);
 yguess = ones(12, 1);
 solinit = bvpinit(tmesh, yguess);
 options = bvpset('Stats','on','RelTol',1e-12, 'Nmax', 100000);
 
-tic
-sol = bvp5c(@bvpfun, @bvpbc, solinit, options);
-tSolve = toc;
+%-------------------- Calculation and Save Data --------------------%
+sol = bvp4c(@(t, y)bvpfun(t, y, rho), @bvpbc, solinit, options);
 
-
-%-------------------------------------------------------------------%
-%------------------ Calculation and Save Data ----------------------%
-%-------------------------------------------------------------------%
 r = sol.y(1:3, :);
 v = sol.y(4:6, :);
-lambda13 = sol.y(7:9, :);
 lambda46 = sol.y(10:12, :);
-lambda = sol.y(7:12, :);
 mu = zeros(size(r, 2), 1);
-tol = 1e-5;
 
 % Lagrange multiplier
 for i=1:size(r, 2)
-    C = norm(r);
     mu(i) = 1 / (2 * rho^2) * (r(:, i)' * lambda46(:, i) ...
                 - v(:, i)' * v(:, i) - r(:, i)' * M1 * r(:, i) ...
                 - r(:, i)' * M2 * v(:, i));
@@ -68,28 +57,19 @@ end
 for i=1:size(r, 2)
     u(:, i) = 2 * mu(i) * r(:, i) - lambda46(:, i);
 end
+
 dJ = 0.5 .* (vecnorm(u) .* vecnorm(u));
 t = sol.x;
 J = trapz(t, dJ);
-fprintf('J = %f', J);
 
+rNorm = vecnorm(r);
+rmin = min(rNorm);
 
+if rmin < rho_min
+    J = 1e6;
+end
 
-
-
-% State - radius
-x = sol.y(1, :);
-y = sol.y(2, :);
-z = sol.y(3, :);
-r = sqrt(x.^2 + y.^2 + z.^2);
-
-% Control
-u1 = u(1,:);
-u2 = u(2,:);
-u3 = u(3,:);
-u = sqrt(u1.^2 + u2.^2 + u3.^2);
-
-save data\indirect_lag_data.mat rho0 rho x y z u1 u2 u3 r u mu tSolve lambda t J
+end
 
 
 %-------------------------------------------------------------------%
@@ -125,7 +105,7 @@ end
 %-------------------------------------------------------------------%
 %------------------------------ Bounds -----------------------------%
 %-------------------------------------------------------------------%
-function res = bvpbc(y0, yf, rho)
+function res = bvpbc(y0, yf)
 rho0 = 10;
 theta0 = pi;
 thetaf = theta0 + pi / 2;
